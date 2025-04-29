7b117f97c8ca7faa7e7078a10630d98d216dcad2
gwilljr88 <106188785+gwilljr88@users.noreply.github.com>
2025-04-28 21:44:15 -0500

    Update custom_config.h reverting previous commit changes

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -25,10 +25,10 @@ U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &non
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     K00  K01  K02  K03  K04       K05  K06  K07  K08  &u_to_U_GAME \
-     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
-     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
-               K32  K33  K34       K35  K36  K37
+     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  &u_to_U_GAME  XXX \
+     XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+     XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+                    K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
@@ -50,9 +50,9 @@ MIRYOKU_X(GAMENUM,"GNum")
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     K00  K01  K02  K03  K04       K05  K06  K07  K08  &u_to_U_BASE \
-     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
-     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
+     XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  &u_to_U_BASE  XXX \
+     XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
+     XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYERMAPPING_GAMENUM MIRYOKU_MAPPING
